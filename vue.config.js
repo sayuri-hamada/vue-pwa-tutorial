a665d034afe36be0e65fccf842a7c16ec3f86212
sayuri.hamada.zizo <hamada@zizo.ne.jp>
2022-09-06 15:13:36 +0900

    テーマカラー変更

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const { GenerateSW } = require('workbox-webpack-plugin')
+
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  pwa: {
+    name: 'vue-pwa-tutorial',
+    themeColor: '#333',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+  }
 })
